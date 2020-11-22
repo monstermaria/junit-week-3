1b6afc24020668c83cafadcf91e27b5d09d606bd
monstermaria <42701428+monstermaria@users.noreply.github.com>
2020-11-22 18:31:15 +0100

    Fixed all IntelliJ warnings and removed unnecessary code

--- src/test/java/com/jaliansystems/javadriver/examples/swing/ut/LoginDialogTest.java
+++ src/test/java/com/jaliansystems/javadriver/examples/swing/ut/LoginDialogTest.java
@@ -1,10 +1,5 @@
 package com.jaliansystems.javadriver.examples.swing.ut;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
-
 import java.util.List;
 
 import javax.swing.SwingUtilities;
@@ -13,8 +8,6 @@ import javax.swing.text.JTextComponent;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -23,8 +16,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import net.sourceforge.marathon.javadriver.JavaDriver;
 import net.sourceforge.marathon.javadriver.JavaProfile;
-import net.sourceforge.marathon.javadriver.JavaProfile.LaunchMode;
-import net.sourceforge.marathon.javadriver.JavaProfile.LaunchType;
+
+import static org.junit.Assert.*;
 
 
 public class LoginDialogTest {
@@ -33,7 +26,7 @@ public class LoginDialogTest {
     private WebDriver driver;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         login = new LoginDialog() {
             private static final long serialVersionUID = 1L;
 
@@ -46,9 +39,7 @@ public class LoginDialogTest {
             }
         };
         SwingUtilities.invokeLater(() -> login.setVisible(true));
-        JavaProfile profile = new JavaProfile(LaunchMode.EMBEDDED);
-        profile.setLaunchType(LaunchType.SWING_APPLICATION);
-        driver = new JavaDriver(profile);
+        driver = new JavaDriver(new JavaProfile());
     }
 
     @After
@@ -70,22 +61,18 @@ public class LoginDialogTest {
         wait.until(ExpectedConditions.elementToBeClickable(loginBtn));
         loginBtn.click();
         assertTrue(login.isSucceeded());
-        assertTrue(login.getSize() != null);
+        assertNotNull(login.getSize());
     }
 
     @Test
     public void loginCancel() {
-        WebElement user = driver.findElement(By.cssSelector("text-field"));
-        user.sendKeys("bob");
-        WebElement pass = driver.findElement(By.cssSelector("password-field"));
-        pass.sendKeys("secret");
         WebElement cancelBtn = driver.findElement(By.cssSelector("button[text='Cancel']"));
         cancelBtn.click();
         assertFalse(login.isSucceeded());
     }
 
     @Test
-    public void loginInvalid() throws InterruptedException {
+    public void loginInvalid() {
         WebElement user = driver.findElement(By.cssSelector("text-field"));
         user.sendKeys("bob");
         WebElement pass = driver.findElement(By.cssSelector("password-field"));
@@ -97,8 +84,6 @@ public class LoginDialogTest {
         driver.switchTo().window("Invalid Login");
         driver.findElement(By.cssSelector("button[text='OK']")).click();
         driver.switchTo().window("Login");
-        user = driver.findElement(By.cssSelector("text-field"));
-        pass = driver.findElement(By.cssSelector("password-field"));
         assertEquals("", user.getText());
         assertEquals("", pass.getText());
     }
